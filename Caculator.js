574641f931c20a8dd1473242b68b9c85e92dcaa3
sonnguyenece <sonnguyenece@gmail.com>
2020-02-21 15:09:23 +0700

    May tinh

---
+++ Caculator.js
@@ -0,0 +1,16 @@
+function input(value1) {
+   document.getElementById("inputNumber").value+=value1;
+}
+function startButton() {
+  document.getElementById("inputNumber").value=0;
+}
+function equal() {
+    document.getElementById("inputNumber").value = eval(document.getElementById("inputNumber").value);
+
+}
+function percent() {
+    document.getElementById("inputNumber").value=document.getElementById("inputNumber").value/100;
+}
+function sqrtFunction() {
+    document.getElementById("inputNumber").value=Math.sqrt(document.getElementById("inputNumber").value);
+}
